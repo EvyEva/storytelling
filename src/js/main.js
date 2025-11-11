a1123cec1eac62eeac6a30064050c6687ffd05d9
EvyEva <eva.kohli@studentfr.ch>
2025-11-11 11:02:38 +0100

    first day

---
+++ src/js/main.js
@@ -0,0 +1,57 @@
+import { gsap } from "gsap";
+
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".thunder_parallax",
+      start: "top top",
+      // le chiffre de end indiquera la durée du parallax, tout devra se caler dessus
+      end: "3000px bottom",
+      scrub: 0.3, //le chiffre donne un effet plus lisse à l'animation
+      pin: true,
+      markers: true,
+      ease: "linear", //je sais pas si cette ligne est utile
+    },
+  })
+  // j'ai commencé par animer le dernier sceen
+  // pour vérifié en le faisant que le bas de l'image arrive pile en bas de la section quand l'animation est finit,
+  // puis vous pourrez animer les autres screen
+  .to(
+    ".thunder_parallax_1",
+    {
+      top: "-1700px",
+    },
+    0
+  )
+  .to(
+    ".thunder_parallax_2",
+    {
+      top: "-3000px",
+    },
+    0
+  )
+  .to(
+    ".thunder_parallax_3",
+    {
+      top: "-3300px",
+    },
+    0
+  )
+  .to(
+    ".thunder_parallax_4",
+    {
+      top: "-3800px",
+    },
+    0
+  )
+  .to(
+    ".thunder_parallax_5",
+    {
+      top: "-4000px",
+    },
+    0
+  );
