120114bf1fd3e5d0d168478e6117c186370b4777
EvyEva <eva.kohli@studentfr.ch>
2025-11-18 11:36:13 +0100

    essaie correction bug

--- src/js/main.js
+++ src/js/main.js
@@ -4,72 +4,85 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
+/// Parllax
+
 // Début du code qui s'exécute au chargement
 document.addEventListener("DOMContentLoaded", function () {
-  // gsap
-  //   .timeline({
-  //     scrollTrigger: {
-  //       trigger: ".thunder_parallax",
-  //       start: "top top",
-  //       // le chiffre de end indiquera la durée du parallax, tout devra se caler dessus
-  //       end: "bottom bottom",
-  //       scrub: true, //le chiffre donne un effet plus lisse à l'animation
-  //       //   pin: true,
-  //       markers: true,
-  //       ease: "linear", //je sais pas si cette ligne est utile
-  //     },
-  //   })
-  //   //   // j'ai commencé par animer le dernier sceen
-  //   //   // pour vérifié en le faisant que le bas de l'image arrive pile en bas de la section quand l'animation est finit,
-  //   //   // puis vous pourrez animer les autres screen
-  //   .to(
-  //     ".thunder_parallax_1",
-  //     {
-  //       marginTop: "-=10%",
-  //       duration: 7,
-  //     },
-  //     0
-  //   )
-  //   .to(
-  //     ".thunder_parallax_2",
-  //     {
-  //       marginTop: "-=20%",
-  //       duration: 7,
-  //     },
-  //     0
-  //   )
-  //   .to(
-  //     ".thunder_parallax_3",
-  //     {
-  //       marginTop: "-=30%",
-  //       duration: 7,
-  //     },
-  //     0
-  //   )
-  //   .to(
-  //     ".thunder_parallax_4",
-  //     {
-  //       marginTop: "-=10%",
-  //       duration: 7,
-  //     },
-  //     0
-  //   )
-  //   .to(
-  //     ".thunder_parallax_5",
-  //     {
-  //       marginTop: "-=20%",
-  //       duration: 7,
-  //     },
-  //     0
-  //   )
-  //   .to(
-  //     ".thunder_storm",
-  //     {
-  //       marginTop: "-=40%",
-  //       duration: 7,
-  //     },
-  //     0
-  //   );
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".thunder_parallax",
+        start: "top top",
+        // le chiffre de end indiquera la durée du parallax, tout devra se caler dessus
+        end: "bottom bottom",
+        scrub: true, //le chiffre donne un effet plus lisse à l'animation
+        //   pin: true,
+        markers: true,
+        id: "Parallax",
+        ease: "linear",
+        onUpdate: (self) => {
+          // `self` contient des informations sur le ScrollTrigger
+          ScrollTrigger.refresh();
+          console.log("Refresh");
+          // console.log("Progression :", self.progress);
+          // Vous pouvez aussi accéder à d'autres valeurs comme self.direction
+        },
+      },
+    })
+    .to(
+      ".thunder_parallax_1",
+      {
+        marginTop: "-=10%",
+        duration: 7,
+      },
+      0
+    )
+    .to(
+      ".thunder_parallax_2",
+      {
+        marginTop: "-=20%",
+        duration: 7,
+      },
+      0
+    )
+    .to(
+      ".thunder_parallax_3",
+      {
+        marginTop: "-=30%",
+        duration: 7,
+      },
+      0
+    )
+    .to(
+      ".thunder_parallax_4",
+      {
+        marginTop: "-=10%",
+        duration: 7,
+      },
+      0
+    )
+    .to(
+      ".thunder_parallax_5",
+      {
+        marginTop: "-=20%",
+        duration: 7,
+      },
+      0
+    )
+    .to(
+      ".thunder_storm",
+      {
+        marginTop: "-=40%",
+        duration: 7,
+      },
+      0
+    );
+  // .add(function () {
+  //   console.log("Refresh");
+  // ScrollTrigger.refresh();
+  // });
+
+  ///Animations éclairs
 
   // // gsap.from(".thunder_lightning_1", {
   // //   scrollTrigger: {
@@ -95,24 +108,23 @@ document.addEventListener("DOMContentLoaded", function () {
   // //   left: "100vw",
   // // });
 
+  /// Scroll horizontal
+
   gsap.to(".family_page_slider_track", {
     scrollTrigger: {
       trigger: ".family_page_slider_mask",
-      // start c'est le moment ou l'animation commence dans le site,
-      // le premier top c'est par rapport à l'élément concerné,
-      // le deuxieme c'est par rapport à la page
       start: "top top",
-      // l'emplacement de end change la vitesse du scroll horizontal,
-      // plus le chiffre à % est grand, plus il sera lent
       end: "+=400%",
       pin: true,
       scrub: true,
-      // markers: true,
+      markers: true,
+      id: "scroll",
     },
-    // x définit jusqu'ou la section va a droite, on peut le calculer (mais j'ai pas compris comment) ou faire à taton, bonne chance !
     x: "-100%",
     ease: "sine.inOut",
   });
+
+  /// Sabre tombe
 
   gsap
     .timeline({
