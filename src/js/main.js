8c3c9f82a2bad95c6c96a82057a334f1ed79f4b6
EvyEva <eva.kohli@studentfr.ch>
2025-11-19 08:38:20 +0100

    correction mais ne peut pas ajouter d'animation de scroll

--- src/js/main.js
+++ src/js/main.js
@@ -8,19 +8,27 @@ gsap.registerPlugin(ScrollToPlugin);
 
 // /// Parllax
 // Parallax
+
+// // Début du code qui s'exécute au chargement
+// document.addEventListener("DOMContentLoaded", function () {
+//   document
+//     .querySelector(".shop_button")
+//     .addEventListener("click", function (e) {
+//       e.preventDefault();
+//       gsap.to(window, { duration: 1, scrollTo: "#shop" });
+//     });
+
 gsap
   .timeline({
     scrollTrigger: {
       trigger: ".thunder_parallax",
       start: "top top",
-      // le chiffre de end indiquera la durée du parallax, tout devra se caler dessus
       end: "bottom bottom",
-      scrub: true, //le chiffre donne un effet plus lisse à l'animation
-      //   pin: true,
-      markers: true,
+      scrub: true,
+      // markers: true,
       id: "Parallax",
-      ease: "linear", // Pas utile lorsqu'on utilise scrub
-      invalidateOnRefresh: true, // Recalcule les valeurs start à chaque refresh
+      ease: "linear",
+      invalidateOnRefresh: true,
     },
   })
   .to(
@@ -78,9 +86,7 @@ gsap
     0
   );
 
-// refresh une fois maintenant (au cas où les images sont déjà chargées) et au chargement/redimensionnement/chargement d'image
 ScrollTrigger.refresh();
-// recalculer ScrollTrigger quand la taille de la mise en page/des assets change
 window.addEventListener("load", () => ScrollTrigger.refresh());
 window.addEventListener("resize", () => ScrollTrigger.refresh());
 document
@@ -88,107 +94,6 @@ document
   .forEach((img) =>
     img.addEventListener("load", () => ScrollTrigger.refresh())
   );
-
-// // Début du code qui s'exécute au chargement
-// document.addEventListener("DOMContentLoaded", function () {
-//   document
-//     .querySelector(".shop_button")
-//     .addEventListener("click", function (e) {
-//       e.preventDefault();
-//       gsap.to(window, { duration: 1, scrollTo: "#shop" });
-//     });
-
-//   gsap
-//     .timeline({
-//       scrollTrigger: {
-//         trigger: ".thunder_parallax",
-//         start: "top top",
-//         end: "bottom bottom",
-//         scrub: true,
-//         // markers: true,
-//         id: "Parallax",
-//         ease: "linear",
-//         onUpdate: (self) => {},
-//       },
-//     })
-//     .to(
-//       ".thunder_parallax_1",
-//       {
-//         marginTop: "-=10%",
-//         duration: 7,
-//       },
-//       0
-//     )
-//     .to(
-//       ".thunder_parallax_2",
-//       {
-//         marginTop: "-=20%",
-//         duration: 7,
-//       },
-//       0
-//     )
-//     .to(
-//       ".thunder_parallax_3",
-//       {
-//         marginTop: "-=30%",
-//         duration: 7,
-//       },
-//       0
-//     )
-//     .to(
-//       ".thunder_parallax_4",
-//       {
-//         marginTop: "-=10%",
-//         duration: 7,
-//       },
-//       0
-//     )
-//     .to(
-//       ".thunder_parallax_5",
-//       {
-//         marginTop: "-=20%",
-//         duration: 7,
-//       },
-//       0
-//     )
-//     .to(
-//       ".thunder_storm",
-//       {
-//         marginTop: "-=40%",
-//         duration: 7,
-//       },
-//       0
-//     );
-//   // .add(function () {
-//   //   console.log("Refresh");
-//   // ScrollTrigger.refresh();
-//   // });
-
-//   ///Animations éclairs
-
-//   // gsap.from(".thunder_lightning_1", {
-//   //   scrollTrigger: {
-//   //     trigger: ".thunder_lightning_1",
-//   //     start: "center top",
-//   //     end: "bottom top",
-//   //     scrub: 1,
-//   //     markers: true,
-//   //     id: "droite",
-//   //   },
-//   //   right: "10vw",
-//   // });
-
-//   // gsap.from(".thunder_lightning_2", {
-//   //   scrollTrigger: {
-//   //     trigger: ".thunder_lightning_2",
-//   //     start: "bottom top",
-//   //     end: "bottom top",
-//   //     scrub: 1,
-//   //     markers: true,
-//   //     id: "gauche",
-//   //   },
-//   //   left: "10vw",
-//   // });
 
 /// Scroll horizontal
 
